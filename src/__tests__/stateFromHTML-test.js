b605b1a4f9205bc2bc4d460f11fe1f46f33d5d8d
Simon Sturmer <sstur@me.com>
2016-04-17 22:45:27 +0700

    update deps and fix eslint

--- src/__tests__/stateFromHTML-test.js
+++ src/__tests__/stateFromHTML-test.js
@@ -18,7 +18,7 @@ describe('stateFromHTML', () => {
 
 function removeKeys(blocks) {
   return blocks.map((block) => {
-    let {key, ...other} = block;
+    let {key, ...other} = block; // eslint-disable-line no-unused-vars
     return other;
   });
 }
